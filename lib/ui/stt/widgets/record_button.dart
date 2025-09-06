f9ed61ca7f6bea926577c7ac4ad8a3843eea3b9f
DELL <sjjbsayfiddinov@gmail.com>
2025-09-06 11:22:01 +0500

    added lottie, ad and other bugs fixed

--- lib/ui/stt/widgets/record_button.dart
+++ lib/ui/stt/widgets/record_button.dart
@@ -1,6 +1,7 @@
 import 'package:audio_waveforms/audio_waveforms.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
 class RecordButton extends StatefulWidget {
   final bool isRecording;
@@ -18,61 +19,37 @@ class RecordButton extends StatefulWidget {
   State<RecordButton> createState() => _RecordButtonState();
 }
 
-class _RecordButtonState extends State<RecordButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    )..repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _RecordButtonState extends State<RecordButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: widget.onTap,
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 300),
-        transitionBuilder:
-            (child, animation) =>
-                ScaleTransition(scale: animation, child: child),
-        child:
-            widget.isRecording
-                ? AudioWaveforms(
-                  size: Size(100.w, 100.w),
-                  recorderController: widget.recorderController,
-                  waveStyle: const WaveStyle(),
-                  enableGesture: false,
-                )
-                : Container(
-                  key: const ValueKey("mic"),
-                  height: 100.w,
-                  width: 100.w,
-                  margin: EdgeInsets.only(bottom: 16.h),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withValues(alpha: 1),
-                        blurRadius: 10,
-                        spreadRadius: 3,
-                      ),
-                    ],
-                  ),
-                  child: const Icon(Icons.mic_none, size: 50),
+      child:
+          widget.isRecording
+              ? Lottie.asset(
+                'assets/lottie/recording.json',
+                height: 200.w,
+                width: 200.w,
+                fit: BoxFit.cover,
+              )
+              : Container(
+                key: const ValueKey("mic"),
+                height: 100.w,
+                width: 100.w,
+                margin: EdgeInsets.only(bottom: 50.h),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withValues(alpha: 1),
+                      blurRadius: 10,
+                      spreadRadius: 5,
+                    ),
+                  ],
                 ),
-      ),
+                child: const Icon(Icons.mic_none, size: 50),
+              ),
     );
   }
 }
